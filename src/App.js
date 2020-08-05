8cb109db01e1449ec7f97b5116cbfa04b08792d8
Andrea Relova <andreamrelova@gmail.com>
2020-08-05 19:05:56 -0400

    fixed tabs

--- src/App.js
+++ src/App.js
@@ -154,32 +154,32 @@ class Graph {
     // addVertex(v)
     addVertex(v)
     {
-    // initialize the adjacent list with a
-    // null array
-    this.AdjList.set(v, []);
+      // initialize the adjacent list with a
+      // null array
+      this.AdjList.set(v, []);
     }
 
     // addEdge(v, w)
     addEdge(v, w)
     {
-    // get the list for vertex v and put the
-    // vertex w denoting edge between v and w
-    this.AdjList.get(v).push(w);
+      // get the list for vertex v and put the
+      // vertex w denoting edge between v and w
+      this.AdjList.get(v).push(w);
 
-    // Since graph is undirected,
-    // add an edge from w to v also
-    this.AdjList.get(w).push(v);
+      // Since graph is undirected,
+      // add an edge from w to v also
+      this.AdjList.get(w).push(v);
     }
 
     // printGraph()
     printGraph()
     {
-    // get all the vertices
-    var get_keys = this.AdjList.keys();
+      // get all the vertices
+      var get_keys = this.AdjList.keys();
 
-    // iterate over the vertices
-    for (var i of get_keys)
-    {
+      // iterate over the vertices
+      for (var i of get_keys)
+      {
         // great the corresponding adjacency list
         // for the vertex
         var get_values = this.AdjList.get(i);
@@ -192,11 +192,8 @@ class Graph {
 
         // print the vertex and its adjacency list
         console.log(i + " -> " + conc);
+      }
     }
-    }
-
-    // bfs(v)
-    // dfs(v)
 }
 
 function Dijsktras() {
