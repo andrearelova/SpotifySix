2d6b059bbbb2e2f2241bb9e34bf703dcc2d6f027
Andrea Relova <andreamrelova@gmail.com>
2020-08-05 20:50:46 -0400

    created queue class

--- src/App.js
+++ src/App.js
@@ -248,5 +248,33 @@ class Graph {
    }
 }
 
+class Queue {
+  //Array is used to implement a Queue
+  constructor()
+  {
+      this.items = [];
+  }
 
+  // enqueue function
+  enqueue(element)
+  {
+    // adding element to the queue
+    this.items.push(element);
+  }
+
+  // dequeue function
+  dequeue()
+  {
+    // removing element from the queue
+    // returns underflow when called
+    // on empty queue
+    if(this.isEmpty())
+        return "Underflow";
+    return this.items.shift();
+  }
+
+  sizeof() {
+    return this.items.length;
+  }
+}
 export default App;
