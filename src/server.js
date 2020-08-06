0c418f69c4dd0da9ff4b0c8c2b1e5ce79f336332
Caleb O'Malley <cjomalley14@yahoo.com>
2020-08-05 21:54:03 -0400

    trying to make server.js work
    implementing functions that will aid in the creation/usage of the graph + dijkstra's algorithm to find the shortest path between two artists.

--- src/server.js
+++ src/server.js
@@ -8,10 +8,11 @@ const fetch = require('node-fetch');
 
 const app = express();
 app.use(cors());
-const btoa = function(str){ return Buffer.from(str).toString('base64'); }
+const btoa = function (str) {
+	return Buffer.from(str).toString('base64');
+}
 
 const jsonParser = bodyParser.json();
-// const urlencodedParser = bodyParser.urlencoded({extended: false});
 const filter = new Filter();
 
 // application parameters (gotta make these secret somehow)
@@ -43,7 +44,7 @@ app.post('/submissions', jsonParser, (req, res) => {
 	}
 
 	// algorithm starts here
-	degree = findDegree(result.artist1, result.artist2);
+	degree = findDegree(result.artist1, result.artist2, accessToken);
 	result.deg = degree;
 
 	res.json(result);
@@ -51,59 +52,161 @@ app.post('/submissions', jsonParser, (req, res) => {
 
 // where most of the degrees of separation work happens
 function findDegree(artist1, artist2) {
-	firstArtist = getArtistInfo(convertToURL(artist1), accessToken);
-	secondArtist = getArtistInfo(convertToURL(artist2), accessToken);
+	console.log("doing first artist");
 
-	return 10;
+	let artist = '';
+	let albums = [];
+	let url1 = convertToURL(artist1);
+	let artists = new Set();
+
+	let target = '';
+	let targetURL = convertToURL(artist2);
+	// gets the ID of the second artist that we will be looking to match
+	fetch(`https://api.spotify.com/v1/search?query=${targetURL}&type=artist&limit=1`, {
+			method: 'GET',
+			headers: {
+				'Authorization': 'Bearer ' + accessToken
+			}
+		}).then((response) => response.json())
+		.then((data) => {
+			target = data.artists.items[0].id;
+			console.log("fetched target artist id: " + target);
+		});
+
+	// gets the first artist's artist ID
+	fetch(`https://api.spotify.com/v1/search?query=${url1}&type=artist&limit=1`, {
+			method: 'GET',
+			headers: {
+				'Authorization': 'Bearer ' + accessToken
+			}
+		}).then((response) => response.json())
+		.then((data) => {
+			artist = data.artists.items[0].id;
+			console.log("fetched first artist id: " + artist);
+
+			// search the artist's albums
+			fetch(`https://api.spotify.com/v1/artists/${artist}/albums`, {
+					method: 'GET',
+					headers: {
+						'Authorization': 'Bearer ' + accessToken
+					}
+				}).then((response) => response.json())
+				.then((albumData) => {
+
+					// for every album by the artist
+					for (let i = 0; i < albumData.items.length; ++i) {
+						albums[i] = albumData.items[i].id;
+						console.log(albumData.items[i].name);
+						let current = albums[i];
+
+						// get the tracks for the album
+						fetch(`https://api.spotify.com/v1/albums/${current}/tracks`, {
+								method: 'GET',
+								headers: {
+									'Authorization': 'Bearer ' + accessToken
+								}
+							}).then((response) => response.json())
+							.then((trackData) => {
+								console.log("tracks: " + trackData.items.length);
+
+								// for every track on the album
+								for (let j = 0; j < trackData.items.length; ++j) {
+									// for every artist on the track, add it
+									for (let k = 0; k < trackData.items[j].artists.length; ++k) {
+										artists.add(trackData.items[j].artists[k].id);
+									}
+									console.log(artists);
+								}
+							});
+					}
+				});
+		});
+
+	return 10; // this is the number returned to the user
 }
 
 function convertToURL(artist) {
 	let artistURL = artist.replace(/ /g, '+');
-	
 	console.log(`artist url: ${artistURL}`);
 	return artistURL;
 }
 
-function getArtistInfo(artist, token) {
-	result = {}
-	fetch(`https://api.spotify.com/v1/search?query=${artist}&type=artist&limit=1`, {
-		method: 'GET',
-		headers: { 'Authorization' : 'Bearer ' + token }
-	}).then((response) => response.json())
-	.then((data) => {
-		result = data.artists.items[0];
-		console.log(result);
-	});
-
-	return result;
+// converts string of text from user to a Spotify Artist ID
+function getArtistID(artist) {
+	let url = convertToURL(artist);
+	fetch(`https://api.spotify.com/v1/search?query=${url}&type=artist&limit=1`, {
+			method: 'GET',
+			headers: {
+				'Authorization': 'Bearer ' + accessToken
+			}
+		}).then((response) => response.json())
+		.then((data) => {
+			return data.artists.items[0].id;
+		});
 }
 
 // searches the spotify database
-function getRelatedArtists(artistID, token){
-	let relatedArtists = [];
-	fetch(`https://api.spotify.com/v1/artists/${artistID}/albums`, {
-		method: 'GET',
-		headers: { 'Authorization' : 'Bearer ' + token }
-	}).then((response) => response.json())
-	.then((data) => {
-		console.log(data);
-	});
+function getSongID(artistID) {
+
+}
+
+function getRelatedArtists(artist) {
+
+	let artists = new Set();
+	// search the artist's albums
+	fetch(`https://api.spotify.com/v1/artists/${artist}/albums`, {
+			method: 'GET',
+			headers: {
+				'Authorization': 'Bearer ' + accessToken
+			}
+		}).then((response) => response.json())
+		.then((albumData) => {
+
+			// for every album by the artist
+			for (let i = 0; i < albumData.items.length; ++i) {
+				albums[i] = albumData.items[i].id;
+				console.log(albumData.items[i].name);
+				let current = albums[i];
+
+				// get the tracks for the album
+				fetch(`https://api.spotify.com/v1/albums/${current}/tracks`, {
+						method: 'GET',
+						headers: {
+							'Authorization': 'Bearer ' + accessToken
+						}
+					}).then((response) => response.json())
+					.then((trackData) => {
+						console.log("tracks: " + trackData.items.length);
+
+						// for every track on the album
+						for (let j = 0; j < trackData.items.length; ++j) {
+							// for every artist on the track, add it
+							for (let k = 0; k < trackData.items[j].artists.length; ++k) {
+								artists.add(trackData.items[j].artists[k].id);
+							}
+						}
+					});
+			}
+		});
+}
+
+return 10; // this is the number returned to the user
 }
 
 // gets the access token for the user
 function getToken() {
 	fetch('https://accounts.spotify.com/api/token', {
-		method: 'POST',
-		headers: {
-			'Content-Type' : 'application/x-www-form-urlencoded',
-			'Authorization' : 'Basic ' + btoa(clientID + ':' + clientSecret)
-		},
-		body: 'grant_type=client_credentials'
-	}).then((response) => response.json())
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Authorization': 'Basic ' + btoa(clientID + ':' + clientSecret)
+			},
+			body: 'grant_type=client_credentials'
+		}).then((response) => response.json())
 		.then((clientData) => {
 			accessToken = clientData.access_token;
 			console.log(clientData);
-		  });
+		});
 }
 
 app.listen(5000, () => console.log('Server running on port 5000...'))
