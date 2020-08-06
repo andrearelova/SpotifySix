3798b862300acded7306215f9df15f8d8b6d809f
Andrea Relova <andreamrelova@gmail.com>
2020-08-05 20:58:13 -0400

    deleted print graph

--- src/App.js
+++ src/App.js
@@ -183,29 +183,6 @@ class Graph {
       this.AdjList.get(w).push(v, songID);
     }
 
-    //probably delete later
-    printGraph()
-    {
-      // get all the vertices
-      var get_keys = this.AdjList.keys();
-
-      // iterate over the vertices
-      for (var i of get_keys)
-      {
-        // great the corresponding adjacency list
-        // for the vertex
-        var get_values = this.AdjList.get(i);
-        var conc = "";
-
-        // iterate over the adjacency list
-        // concatenate the values into a string
-        for (var j of get_values)
-            conc += j + " ";
-
-        // print the vertex and its adjacency list
-      }
-    }
-
     BreadthFirst(startingNode, endNode)
     {
       // create a visited array
